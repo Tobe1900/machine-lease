72810097bca6cc99a7bdd6c612cc6019694b76ae
Tobe1900 <cosem_520@163.com>
2020-01-14 23:19:04 +0800

    login api

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //app.js
+import config from './config/index.js'
+const app = getApp()
 
 App({
   onLaunch: function () {
@@ -7,6 +9,26 @@ App({
       success: res => {
         if(res.code){
           // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          wx.request({
+            url: config.requesUrl + 'onLogin',
+            data:{
+              code: res.code
+            },
+            header: { 'content-type': 'application/json' },
+            method:'POST',
+            success:function(res){
+              if(!res.errcode){
+                app.globalData.token = res.data.token
+                wx.setStorageSync('token', res.data.token)
+              } else {
+                wx.showToast({
+                  title: res.errmsg,
+                  icon: 'none',
+                  duration: 2000
+                })
+              }
+            }
+          })
         }
       }
     })
@@ -14,6 +36,7 @@ App({
   globalData: {
     userInfo: null,
     city:'',
-    hasLogin: false
+    hasLogin: false,
+    token: undefined
   }
 })
---
+++ config/index.js
@@ -0,0 +1,5 @@
+const config = {
+  requestUrl: 'https://www.m-th.cn/jsd/api/'
+}
+
+module.exports = config
